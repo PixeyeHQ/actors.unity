e213ae78bab3cc608b9a6f68e3be4c074f41a100
PIX\Pixeye <trqe@mail.ru>
2018-04-28 13:05:07 +0200

    Update

---
+++ Assets/[0]Common/Engine/Interfaces/IRandom.cs
@@ -0,0 +1,15 @@
+/*===============================================================
+Product:    Unity3dTools
+Developer:  Dimitry Pixeye - info@pixeye,games
+Company:    Homebrew
+Date:       4/27/2018 8:04 PM
+================================================================*/
+
+
+namespace Homebrew
+{
+	public interface IRandom
+	{
+		float returnChance { get; }
+	}
+}
