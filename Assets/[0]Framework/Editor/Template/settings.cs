9ad15fdf0a924179ea04234f0965ae2de69a5546
CrayzySh0wman <stopgame@ya.ru>
2018-09-27 03:03:07 +0700

    Revert "Template 1.1"
    This reverts commit 11ef0793453a595459d3b28049db57e832895e2b.

--- Assets/[0]Framework/Editor/Template/settings.cs
+++ Assets/[0]Framework/Editor/Template/settings.cs
@@ -8,22 +8,10 @@ using UnityEngine;
 //template genent settings
 public class settings : EditorWindow
 {
-    static readonly int version = 100;
 
-    // public static string nameProgect;
-
+    public static string nameProgect;
     public static string xmlPath = @"Assets\[0]Framework\Editor\Template\Settings.xml";
     public static XmlDocument xmlDoc = new XmlDocument();
-
-
-    private Vector2 scrollViewVector = Vector2.zero;
-    public Rect dropDownRect = new Rect(220, 10, 125, 300);
-    public static string[] list = new string[6] { "None", "slot_1", "slot_2", "slot_3", "slot_4", "slot_5" };
-
-    static int indexNumber;
-    bool show = false;
-
-    static int fileVersion;
 
     //creating the settings window
     [MenuItem("Tools/Actors/Settings")]
@@ -34,137 +22,42 @@ public class settings : EditorWindow
 
         //check for xml file
         if (!File.Exists(xmlPath)) CreateFile(xmlPath);
-
-        xmlDoc.Load(xmlPath);
-
-        //checking the current version of the xml file
-        if (xmlDoc.SelectSingleNode("Settings/Version") == null) CreateFile(xmlPath);
-        else
         {
-            fileVersion = int.Parse(xmlDoc.GetElementsByTagName("namespace").Item(0).Attributes.Item(0).Value);
-            if (fileVersion != version) CreateFile(xmlPath);
+            xmlDoc.Load(xmlPath);
+            nameProgect = xmlDoc.SelectSingleNode("Settings/namespace").InnerText;
         }
-
-        //index of the selected project name
-        indexNumber = int.Parse(xmlDoc.GetElementsByTagName("namespace").Item(0).Attributes.Item(0).Value);
-
-        //checking the list of project names
-        var Index = xmlDoc.GetElementsByTagName("List");
-        for (int i = 0; i < Index.Count; i++)
-        {
-            list[i] = Index[i].Attributes.Item(0).Value;
-        }
-
     }
 
     void OnGUI()
     {
 
-        GUI.Label(new Rect((dropDownRect.x - 180), dropDownRect.y, dropDownRect.width, 20), "NameSpace: ");
+        nameProgect = EditorGUILayout.TextField("NameSpace: ", nameProgect);
 
-        if (GUI.Button(new Rect((dropDownRect.x - 100), dropDownRect.y, dropDownRect.width, 18), ""))
+        //save the settings to a file
+        if (GUILayout.Button("Save Settings!"))
         {
-            if (!show)
-            {
-                show = true;
-            }
-            else
-            {
-                show = false;
-                RefreshList(indexNumber);
-            }
+            Debug.Log("Save Settings!");
+            xmlDoc.Load(xmlPath);
+            nameProgect = xmlDoc.SelectSingleNode("Settings/namespace").InnerText = nameProgect;
+            xmlDoc.Save(xmlPath);
+            AssetDatabase.Refresh();
         }
-        //display the drop-down list
-        if (show)
-        {
-            scrollViewVector = GUI.BeginScrollView(new Rect((dropDownRect.x - 100), (dropDownRect.y + 20), dropDownRect.width, dropDownRect.height), scrollViewVector, new Rect(0, 0, dropDownRect.width, Mathf.Max(dropDownRect.height, (list.Length * 25))));
-
-            GUI.Box(new Rect(0, 0, dropDownRect.width, Mathf.Max(dropDownRect.height, (list.Length * 25))), "");
-
-            for (int index = 0; index < list.Length; index++)
-            {
-
-                if (index != 0) list[index] = GUI.TextField(new Rect(0, (20 + index * 40), dropDownRect.height, 20), list[index], 25);
-
-                if (GUI.Button(new Rect(0, (index * 40), dropDownRect.height, 20), ""))
-                {
-                    show = false;
-                    indexNumber = index;
-                    RefreshList(index);
-                }
-                GUI.Label(new Rect(5, (index * 40), dropDownRect.height, 20), list[index]);
-
-            }
-
-            GUI.EndScrollView();
-        }
-        else
-        {
-            GUI.Label(new Rect((dropDownRect.x - 95), dropDownRect.y, 300, 20), list[indexNumber]);
-        }
-
-
     }
-
     // creating xml file
     static void CreateFile(string xmlPath)
     {
-        xmlDoc = new XmlDocument();
-
-        XmlNode Node;
-        XmlAttribute attribute;
-
         XmlNode MainNode = xmlDoc.CreateElement("Settings");
         xmlDoc.AppendChild(MainNode);
 
-        Node = xmlDoc.CreateElement("Version");
-        Node.InnerText = version.ToString();
-        MainNode.AppendChild(Node);
+        XmlNode Node;
 
         Node = xmlDoc.CreateElement("namespace");
         Node.InnerText = "None";
-        MainNode.AppendChild(Node);
-
-        attribute = xmlDoc.CreateAttribute("Index");
-        attribute.Value = "0";
-        Node.Attributes.Append(attribute);
-
-        for (int i = 0; i < 6; i++)
-        {
-            Node = xmlDoc.CreateElement("List");
-            Node.InnerText = "Slot_" + i;
-
-            MainNode.AppendChild(Node);
-
-            attribute = xmlDoc.CreateAttribute("Name");
-            if (i == 0) attribute.Value = "None";
-            else attribute.Value = "Slot_" + i.ToString();
-            Node.Attributes.Append(attribute);
-        }
+        Node.AppendChild(Node);
 
         xmlDoc.Save(xmlPath);
 
         AssetDatabase.Refresh();
-    }
-    //updating the list of project names
-    static void RefreshList(int index)
-    {
 
-        xmlDoc.Load(xmlPath);
-
-        xmlDoc.SelectSingleNode("Settings/namespace").InnerText = list[index];
-        xmlDoc.GetElementsByTagName("namespace").Item(0).Attributes.Item(0).Value = index.ToString();
-
-        var Index = xmlDoc.GetElementsByTagName("List");
-
-        for (int i = 1; i < Index.Count; i++)
-        {
-            Index[i].Attributes.Item(0).Value = list[i];
-
-        }
-
-        xmlDoc.Save(xmlPath);
-
-        AssetDatabase.Refresh();
     }
 }
