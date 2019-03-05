8882dfdc045dc705bc935c17bf412387a8200af3
Pixeye <dimmpixeye@users.noreply.github.com>
2019-03-05 08:03:15 +0200

    Update

---
+++ Assets/Runtime/Source/DataGameSettings.cs
@@ -0,0 +1,12 @@
+﻿//   Project : Actors
+//  Contacts : Pixeye - ask@pixeye.games 
+
+using UnityEngine;
+
+namespace Pixeye
+{
+    [CreateAssetMenu(fileName = "DataGameSettings", menuName = "Actors/Data/DataGameSettings")]
+    public class DataGameSettings : DataGame, IKernel
+    {
+    }
+}
---
+++ Assets/Runtime/Tags/Tag.cs
@@ -0,0 +1,9 @@
+//   Project : Actors
+//  Contacts : Pixeye - ask@pixeye.games 
+
+namespace Pixeye
+{
+	public static class Tag
+	{
+	}
+}
