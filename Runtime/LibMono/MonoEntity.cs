116c98b4fde8918e5dfb30938b36ce8dec87231d
Pixeye <dimmpixeye@users.noreply.github.com>
2019-06-01 21:12:34 +0200

    Update

--- Runtime/LibMono/MonoEntity.cs
+++ Runtime/LibMono/MonoEntity.cs
@@ -8,12 +8,37 @@ namespace Pixeye.Framework
 	sealed public class MonoEntity : MonoBehaviour, IActor
 	{
 
-		public ent entity;
+		#if UNITY_EDITOR
 
+		[SerializeField]
+		int _entity;
+
+		ent entityCached;
+
+		public ent entity
+		{
+			get
+			{
+				return entityCached;
+			}
+			set
+			{
+				entityCached = value;
+				_entity = entityCached.id;
+			}
+		}
 		public ref ent GetEntity()
+		{
+			return ref entityCached;
+		}
+		#else
+			public ent entity;
+
+			public ref ent GetEntity()
 		{
 			return ref entity;
 		}
+		#endif
 
 	}
 }
