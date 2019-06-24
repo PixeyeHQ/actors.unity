d0bd75b2ff3eb78e084ebcba6c3cbe0d1be01540
Pixeye <dimmpixeye@users.noreply.github.com>
2019-06-24 16:35:34 +0200

    Update

---
+++ Runtime/LibEcs/BufferObj.cs
@@ -0,0 +1,52 @@
+//  Project : ecs.unity
+// Contacts : Pix - ask@pixeye.games
+
+using System;
+
+namespace Pixeye.Source
+{
+	sealed class BufferObj<T> where T : class
+	{
+		public static BufferObj<T> Default = null;
+		public static Func<T> Creator;
+		public T[] source;
+		public int length;
+
+		public ref T this[int index] => ref source[index];
+
+
+		public BufferObj(int size, Func<T> Creator)
+		{
+			source               = new T[size];
+			BufferObj<T>.Creator = Creator;
+		}
+
+		public T Get()
+		{
+			if (length == source.Length)
+				Array.Resize(ref source, length << 1);
+
+			ref var next = ref source[length++];
+			if (next == null)
+				next = Creator();
+
+			return next;
+		}
+
+
+		public void Add(in T obj)
+		{
+			if (length == source.Length)
+				Array.Resize(ref source, length << 1);
+
+			source[length++] = obj;
+		}
+
+		public void RemoveAt(int index)
+		{
+			--length;
+			for (int i = index; i < length; ++i)
+				source[i] = source[i + 1];
+		}
+	}
+}
