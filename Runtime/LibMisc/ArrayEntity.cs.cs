7efbcc9804bfede40b0c4af0d11bdc63b41edae2
Pixeye <dimmpixeye@users.noreply.github.com>
2019-08-24 22:37:25 +0200

    Update

---
+++ Runtime/LibMisc/ArrayEntity.cs.cs
@@ -0,0 +1,45 @@
+//  Project : ecs
+// Contacts : Pix - ask@pixeye.games
+
+using System;
+using Unity.IL2CPP.CompilerServices;
+
+
+namespace Pixeye.Framework
+{
+	[Serializable]
+	public struct ArrayEntity
+	{
+		public int length;
+		public ent[] source;
+
+		public ref ent this[int index]
+		{
+			get => ref source[index];
+		}
+
+		public ArrayEntity(int size)
+		{
+			source = new ent[size];
+			length = 0;
+		}
+
+		public void Add(in ent entity)
+		{
+			source[length++] = entity;
+		}
+
+		public void Remove(in ent entity)
+		{
+			for (int i = 0; i < length; i++)
+			{
+				ref var val = ref source[i];
+				if (entity.EqualsAndExist(val))
+				{
+					Array.Copy(source, i + 1, source, i, --length - i);
+					break;
+				}
+			}
+		}
+	}
+}
