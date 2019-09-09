53b25caba3396dcb6ed343b70b9b42a9fa2597a7
Pixeye <dimmpixeye@users.noreply.github.com>
2019-09-09 05:56:41 +0200

    Update HelperArray.cs

--- Runtime/Helpers/HelperArray.cs
+++ Runtime/Helpers/HelperArray.cs
@@ -103,7 +103,38 @@ namespace Pixeye.Framework
 
 			return res;
 		}
-		
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static T[] Slice<T>(this T[] source, Predicate<T> predicate, int end)
+		{
+			var          start     = -1;
+
+			for (int i = 0; i < source.Length; i++)
+			{
+				ref var val = ref source[i];
+				if (predicate(val))
+				{
+					start = i;
+					break;
+				}
+			}
+
+			#if UNITY_EDITOR
+			if (start == -1)
+				throw new Exception($"Couldn't find object typeof {typeof(T)}");
+			#endif
+
+			int len = start + end - start;
+
+			// Return new array.
+			var res = new T[len];
+			for (int i = 0; i < len; i++)
+			{
+				res[i] = source[i + start];
+			}
+
+			return res;
+		}
 		
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static int BinarySearch(ref int[] entries, int value, int left, int right)
