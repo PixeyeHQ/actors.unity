efdd1bc46eb1f81832e2df266c9b717b1c339ea4
Pixeye <dimmpixeye@users.noreply.github.com>
2019-09-10 13:56:50 +0200

    Update Obj.cs

--- Runtime/Helpers/Obj.cs
+++ Runtime/Helpers/Obj.cs
@@ -24,6 +24,9 @@ namespace Pixeye.Framework
 			var tr = Object.Instantiate(prefab).transform;
 			tr.position = startPosition;
 			tr.localRotation = startRotation;
+			#if UNITY_EDITOR
+			tr.SetAsLastSibling();
+			#endif
 			return tr;
 		}
 
@@ -32,16 +35,33 @@ namespace Pixeye.Framework
 			var tr = Object.Instantiate(prefab, parent).transform;
 			tr.localPosition = startPosition;
 			tr.localRotation = startRotation;
+			#if UNITY_EDITOR
+			tr.SetAsLastSibling();
+			#endif
 			return tr;
 		}
 		public static T Spawn<T>(GameObject prefab, Transform parent , Vector3 startPosition = default, Quaternion startRotation = default)
 		{
+		  
+			#if UNITY_EDITOR
+      var o = Spawn(prefab, parent, startPosition, startRotation).GetComponentInChildren<T>();
+			(o as Transform).SetAsLastSibling();
+			return o;
+			#else
 			return Spawn(prefab, parent, startPosition, startRotation).GetComponentInChildren<T>();
+			#endif
 		}
 
 		public static T Spawn<T>(GameObject prefab, Vector3 startPosition = default, Quaternion startRotation = default)
 		{
+		 
+			#if UNITY_EDITOR
+      var o = Spawn(prefab, startPosition, startRotation).GetComponentInChildren<T>();
+			(o as Transform).SetAsLastSibling();
+	return o;
+			#else
 			return Spawn(prefab, startPosition, startRotation).GetComponentInChildren<T>();
+			#endif
 		}
 		
 		//Pooled
@@ -50,16 +70,33 @@ namespace Pixeye.Framework
 			var tr = Pool.pools[poolID].Spawn(prefab, parent).transform;
 			tr.localPosition = startPosition;
 			tr.localRotation = startRotation;
+			#if UNITY_EDITOR
+			tr.SetAsLastSibling();
+			#endif
 			return tr;
 		}
 		public static T Spawn<T>(int poolID, GameObject prefab, Transform parent , Vector3 startPosition = default, Quaternion startRotation = default)
 		{
+	 
+			#if UNITY_EDITOR
+      var o = Spawn(poolID, prefab, parent, startPosition, startRotation).GetComponentInChildren<T>();
+			(o as Transform).SetAsLastSibling();
+		return o;
+			#else
 			return Spawn(poolID, prefab, parent, startPosition, startRotation).GetComponentInChildren<T>();
+			#endif
 		}
 
 		public static T Spawn<T>(int poolID, GameObject prefab, Vector3 startPosition = default, Quaternion startRotation = default)
 		{
-			return Spawn(poolID , prefab, startPosition, startRotation).GetComponentInChildren<T>();
+	 
+			#if UNITY_EDITOR
+      var o = Spawn(poolID , prefab, startPosition, startRotation).GetComponentInChildren<T>();
+			(o as Transform).SetAsLastSibling();
+		return o;
+			#else
+			return Spawn(poolID, prefab, startPosition, startRotation).GetComponentInChildren<T>();
+			#endif
 		}
 		
 		public static Transform Spawn(int poolID, GameObject prefab, Vector3 startPosition = default, Quaternion startRotation = default)
@@ -67,6 +104,11 @@ namespace Pixeye.Framework
 			var tr = Pool.pools[poolID].Spawn(prefab).transform;
 			tr.position = startPosition;
 			tr.localRotation = startRotation;
+
+			#if UNITY_EDITOR
+			tr.SetAsLastSibling();
+			#endif
+			
 			return tr;
 		}
 
@@ -80,6 +122,12 @@ namespace Pixeye.Framework
 			var tr = Object.Instantiate(Box.Get<GameObject>(prefabID)).transform;
 			tr.position = startPosition;
 			tr.localRotation = startRotation;
+
+      #if UNITY_EDITOR
+			tr.SetAsLastSibling();
+      #endif
+			
+			
 			return tr;
 		}
 
@@ -88,15 +136,35 @@ namespace Pixeye.Framework
 			var tr = Object.Instantiate(Box.Get<GameObject>(prefabID), parent).transform;
 		 	tr.localPosition = startPosition;
 	  	tr.localRotation = startRotation;
+
+	    #if UNITY_EDITOR
+			tr.SetAsLastSibling();
+	    #endif
+	    
 			return tr;
 		}
 		public static T Spawn<T>(string prefabID, Transform parent , Vector3 startPosition = default, Quaternion startRotation = default)
 		{
+			 
+			#if UNITY_EDITOR
+      var o = Spawn(prefabID, parent, startPosition, startRotation).GetComponentInChildren<T>();
+			(o as Transform).SetAsLastSibling();
+		return o;
+			#else
 			return Spawn(prefabID, parent, startPosition, startRotation).GetComponentInChildren<T>();
+			#endif
 		}
 		public static T Spawn<T>(string prefabID, Vector3 startPosition = default, Quaternion startRotation = default)
 		{
 			return Spawn(prefabID, startPosition, startRotation).GetComponentInChildren<T>();
+
+			#if UNITY_EDITOR
+      var o = Spawn(prefabID, startPosition, startRotation).GetComponentInChildren<T>();
+			(o as Transform).SetAsLastSibling();
+		return o;
+			#else
+			return Spawn(prefabID, startPosition, startRotation).GetComponentInChildren<T>();
+			#endif
 		}
 		
 		
@@ -106,6 +174,11 @@ namespace Pixeye.Framework
 			var tr = Pool.pools[poolID].Spawn(Box.Get<GameObject>(prefabID)).transform;
 			tr.position = startPosition;
 			tr.localRotation = startRotation;
+
+			#if UNITY_EDITOR
+			tr.SetAsLastSibling();
+			#endif
+			
 			return tr;
 		}
 
@@ -114,17 +187,36 @@ namespace Pixeye.Framework
 			var tr = Pool.pools[poolID].Spawn(Box.Get<GameObject>(prefabID), parent).transform;
 			tr.localPosition = startPosition;
 			tr.localRotation = startRotation;
+
+			#if UNITY_EDITOR
+			tr.SetAsLastSibling();
+			#endif
+			
 			return tr;
 		}
 
 		public static T Spawn<T>(int poolID, string prefabID, Transform parent , Vector3 startPosition = default, Quaternion startRotation = default)
 		{
+			#if UNITY_EDITOR
+      var o = Spawn(poolID, prefabID, parent, startPosition, startRotation).GetComponentInChildren<T>();
+			(o as Transform).SetAsLastSibling();
+		return o;
+			#else
 			return Spawn(poolID, prefabID, parent, startPosition, startRotation).GetComponentInChildren<T>();
+			#endif
+		 
 		}
 
 		public static T Spawn<T>(int poolID, string prefabID, Vector3 startPosition = default, Quaternion startRotation = default)
 		{
+			#if UNITY_EDITOR
+      var o = Spawn(poolID, prefabID, startPosition, startRotation).GetComponentInChildren<T>();
+			(o as Transform).SetAsLastSibling();
+      return o;
+			#else
 			return Spawn(poolID, prefabID, startPosition, startRotation).GetComponentInChildren<T>();
+			#endif
+			 
 		}
 	}
 }
