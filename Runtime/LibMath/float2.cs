2d385ed646265a66c9aa7ccf07e162cf4b68978e
Pixeye <dimmpixeye@users.noreply.github.com>
2019-09-18 06:15:36 +0200

    update

---
+++ Runtime/LibMath/float2.cs
@@ -0,0 +1,20 @@
+namespace Pix.Actors.Math
+{
+	public struct float2
+	{
+		public float a;
+		public float b;
+
+		public float2(float a, float b)
+		{
+			this.a = a;
+			this.b = b;
+		}
+
+
+		public float Between(float chance = 0.5f)
+		{
+			return UnityEngine.Random.value > chance ? a : b;
+		}
+	}
+}
