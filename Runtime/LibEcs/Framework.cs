613f527303d28e4f4dfb1f2c599f50a69fcefc9e
Pixeye <dimmpixeye@users.noreply.github.com>
2020-03-09 16:50:39 +0200

    Update Framework.cs

--- Runtime/LibEcs/Framework.cs
+++ Runtime/LibEcs/Framework.cs
@@ -62,7 +62,12 @@ namespace Pixeye.Actors
 	{
 		public GroupCore this[int index] => globals[index];
 
-		internal static GroupCore[] globals = new GroupCore[32];
+		public static bool has(int index)
+		{
+			return globals.Length > index && globals[index] != null;
+		}
+
+		public static GroupCore[] globals = new GroupCore[32];
 		internal static CacheGroup All = new CacheGroup();
 		internal static FamilyGroupTags ByTag = new FamilyGroupTags();
 		internal static FamilyGroupTags ByType = new FamilyGroupTags();
