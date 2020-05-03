f103d82158ba16384e99e029ff2275e2a5a8f24d
desendo <djedai5@yandex.ru>
2020-05-03 15:24:52 +0300

    fix using

--- Runtime/LibStarter/Starter.cs
+++ Runtime/LibStarter/Starter.cs
@@ -12,7 +12,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using UnityEngine;
-using Component = System.ComponentModel.Component;
 
 namespace Pixeye.Actors
 {
